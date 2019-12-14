347f2ea5b632fc5eaf37a1a9f8cb094b8c46c4d0
Rachel Evans <revans@zendesk.com>
2019-12-14 13:51:41 +0100

    Refactor machine

--- lib/machine.rb
+++ lib/machine.rb
@@ -1,34 +1,48 @@
 class Machine
-  def self.output_of(input, position = 0)
-    if position < 0 or position >= input.length
+  Halt = Class.new(RuntimeError)
+
+  def initialize(memory)
+    @memory = memory
+    @position = 0
+  end
+
+  attr_reader :memory, :position
+
+  def run
+    while true
+      run_one_instruction
+    end
+  rescue Halt
+  end
+
+  def run_one_instruction
+    if position < 0 or position >= memory.length
       raise 'bad position'
     end
 
-    case input[position]
+    case memory[position]
     when 1
       l_pos, r_pos, o_pos = [1,2,3].map do |i|
-        raise 'bad position' if position+i >= input.length
-        value = input[position+i]
-        raise 'bad position' if value >= input.length
+        raise 'bad position' if position+i >= memory.length
+        value = memory[position+i]
+        raise 'bad position' if value >= memory.length
         value
       end
 
-      output = input.dup
-      output[o_pos] = output[l_pos] + output[r_pos]
-      output_of(output, position+4)
+      memory[o_pos] = memory[l_pos] + memory[r_pos]
+      @position += 4
     when 2
       l_pos, r_pos, o_pos = [1,2,3].map do |i|
-        raise 'bad position' if position+i >= input.length
-        value = input[position+i]
-        raise 'bad position' if value >= input.length
+        raise 'bad position' if position+i >= memory.length
+        value = memory[position+i]
+        raise 'bad position' if value >= memory.length
         value
       end
 
-      output = input.dup
-      output[o_pos] = output[l_pos] * output[r_pos]
-      output_of(output, position+4)
+      memory[o_pos] = memory[l_pos] * memory[r_pos]
+      @position += 4
     when 99
-      input
+      raise Halt
     else
       raise 'unexpected opcode'
     end
--- spec/day02_machine_spec.rb
+++ spec/day02_machine_spec.rb
@@ -2,18 +2,24 @@ require 'machine'
 
 RSpec.describe Machine do
 
+  def output_of(memory)
+    m = Machine.new(memory)
+    m.run
+    m.memory
+  end
+
   describe '.output_of' do
     it 'passes example 1' do
-      expect(Machine.output_of([1,0,0,0,99])).to eq([2,0,0,0,99])
+      expect(output_of([1,0,0,0,99])).to eq([2,0,0,0,99])
     end
     it 'passes example 2' do
-      expect(Machine.output_of([2,3,0,3,99])).to eq([2,3,0,6,99])
+      expect(output_of([2,3,0,3,99])).to eq([2,3,0,6,99])
     end
     it 'passes example 3' do
-      expect(Machine.output_of([2,4,4,5,99,0])).to eq([2,4,4,5,99,9801])
+      expect(output_of([2,4,4,5,99,0])).to eq([2,4,4,5,99,9801])
     end
     it 'passes example 4' do
-      expect(Machine.output_of([1,1,1,4,99,5,6,0,99])).to eq([30,1,1,4,2,5,6,0,99])
+      expect(output_of([1,1,1,4,99,5,6,0,99])).to eq([30,1,1,4,2,5,6,0,99])
     end
   end
 
