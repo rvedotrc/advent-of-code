7e61e0778eb5a7ef86de0d022c80b47e7824536e
Rachel Evans <revans@zendesk.com>
2019-12-14 10:48:27 +0100

    rspec

--- day01/fuel.rb
+++ day01/fuel.rb
@@ -4,13 +4,20 @@ def fuel_for(mass)
   (mass / 3.0).floor - 2
 end
 
-raise "nope" unless fuel_for(12) == 2
-raise "nope" unless fuel_for(14) == 2
-raise "nope" unless fuel_for(1969) == 654
-raise "nope" unless fuel_for(100756) == 33583
+if $0 == __FILE__
+  masses = $stdin.each_line.map do |t|
+    fuel_for(t.chomp.to_f)
+  end
 
-masses = $stdin.each_line.map do |t|
-  fuel_for(t.chomp.to_f)
+  puts masses.reduce(&:+)
+else
+  require 'rspec'
+  RSpec.describe do
+    it 'works' do
+      expect(fuel_for(12)).to eq(2)
+      expect(fuel_for(14)).to eq(2)
+      expect(fuel_for(1969)).to eq(654)
+      expect(fuel_for(100756)).to eq(33583)
+    end
+  end
 end
-
-puts masses.reduce(&:+)
