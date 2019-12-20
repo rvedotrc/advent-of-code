88484c55671787934664c58588070e8b79d50ef0
Rachel Evans <revans@zendesk.com>
2019-12-20 17:15:45 +0000

    Tracks 'to explore'

--- lib/repair_droid.rb
+++ lib/repair_droid.rb
@@ -1,8 +1,10 @@
 require 'machine'
+require 'set'
 
 class RepairDroid
 
   UNKNOWN = '?'
+  EXPLORE = 'E'
   WALL = '#'
   EMPTY = ' '
   CURRENT = 'D'
@@ -12,36 +14,34 @@ class RepairDroid
     @program = program
   end
 
-  attr_reader :program, :map, :position
+  attr_reader :program, :map, :position, :to_explore
 
   def run
     @panels = {}
-    @position = [0, 0]
+    @position = Position.new(0, 0)
     @panels[@position] = nil
+
+    # Places that we can definitely reach, to test what's there
+    @to_explore = Set.new
+    add_unknown(position)
+    position.neighbours.values.each do |neighbour|
+      add_unknown(neighbour)
+    end
 
     machine = Machine::Commandable.new(program)
     machine.start
 
-    1000.times do
+    while true do
       machine.running? or raise 'machine crashed'
 
       puts
       puts *draw
+      puts
+      puts to_explore.inspect
 
-      # north (1), south (2), west (3), and east (4)
-      dir = rand(4) + 1
+      neighbours = position.neighbours
 
-      desired_position = \
-      case dir
-      when 1
-        [position[0], position[1]-1]
-      when 2
-        [position[0], position[1]+1]
-      when 3
-        [position[0]-1, position[1]]
-      when 4
-        [position[0]+1, position[1]]
-      end
+      dir, desired_position = neighbours.entries.shuffle.first
 
       machine.input(dir)
 
@@ -67,6 +67,14 @@ class RepairDroid
     machine.stop
   end
 
+  def add_unknown(pos)
+    if @panels[pos]
+      raise "add_unknown #{pos} but we know what's there"
+    end
+
+    @to_explore << pos
+  end
+
   def draw
     x_values = @panels.keys.map(&:first).sort
     y_values = @panels.keys.map(&:last).sort
@@ -75,6 +83,8 @@ class RepairDroid
       (x_values.min .. x_values.max).map do |x|
         if [x, y] == position
           CURRENT
+        elsif to_explore.include?(Position.new(x, y))
+          EXPLORE
         else
           @panels[[x, y]] || UNKNOWN
         end
@@ -89,7 +99,34 @@ class RepairDroid
       raise "Already found #{already} at #{at} but now found #{what}"
     end
 
+    @to_explore.delete(at)
     @panels[at] = what
+
+    if what == OXYGEN or what == EMPTY
+      at.neighbours.values.each do |neighbour|
+        @to_explore.add(neighbour) unless @panels[neighbour]
+      end
+    end
+  end
+
+  class Position < Array
+    def initialize(x, y)
+      super()
+      self << x
+      self << y
+    end
+
+    def neighbours
+      # north (1), south (2), west (3), and east (4)
+      x, y = self
+
+      {
+        1 => Position.new(x, y - 1),
+        2 => Position.new(x, y + 1),
+        3 => Position.new(x - 1, y),
+        4 => Position.new(x + 1, y),
+      }
+    end
   end
 
 end
