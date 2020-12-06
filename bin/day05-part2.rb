00ba3fd8955d52d87a78dd656ba985a18f85625f
Rachel Evans <git@rve.org.uk>
2020-12-06 20:27:38 +0100

    Day 5

---
+++ bin/day05-part2.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+ids = $stdin.each_line.map do |line|
+  code = line.chomp
+  id = Integer(code.tr('FBLR', '0101'), 2)
+end
+
+last = nil
+ids.sort.each do |id|
+  if last and id == last + 2
+    puts last + 1
+  end
+  last = id
+end
+
+
