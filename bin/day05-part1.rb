5b8bece315957504c838ddbc15a5aa1c8a4f2b40
Rachel Evans <git@rve.org.uk>
2020-12-06 20:43:56 +0100

    Remove unused vars

--- bin/day05-part1.rb
+++ bin/day05-part1.rb
@@ -2,7 +2,7 @@
 
 ids = $stdin.each_line.map do |line|
   code = line.chomp
-  id = Integer(code.tr('FBLR', '0101'), 2)
+  Integer(code.tr('FBLR', '0101'), 2)
 end
 
 p ids
--- bin/day05-part2.rb
+++ bin/day05-part2.rb
@@ -2,10 +2,11 @@
 
 ids = $stdin.each_line.map do |line|
   code = line.chomp
-  id = Integer(code.tr('FBLR', '0101'), 2)
+  Integer(code.tr('FBLR', '0101'), 2)
 end
 
 last = nil
+
 ids.sort.each do |id|
   if last and id == last + 2
     puts last + 1
